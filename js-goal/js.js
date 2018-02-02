a1b4b32f4365e22dc9cd3b9252b9249a776c730f
Melashenko <34339074+Melashenko@users.noreply.github.com>
2018-02-02 19:06:07 +0200

    Add files via upload

---
+++ js-goal/js.js
@@ -0,0 +1,12 @@
+function f2(){
+var gol = document.getElementById('goal').value;
+if(gol==7){
+	alert("Красава ваще,лучший,цьом");
+}
+else if(gol>7){
+	alert("Чё дурак блять,меньше");
+}
+else{
+	alert("Чё дурак блять,больше");
+}
+}
